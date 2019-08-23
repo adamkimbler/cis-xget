c7f0c2240465de70e5c677f6f1278f9c710a1164
Adam Kimbler <akimbler@fiu.edu>
2019-08-23 16:46:24 -0400

    Update xnat_list.py

--- xnat_list.py
+++ xnat_list.py
@@ -8,8 +8,8 @@ import re
 #project = sys.argv[2]
 #ref = sys.argv[3]
 
-def xget_file(config_file=None, project=None, regex=None):
-    xnat_list = []
+def xget_file(config_file=None, project=None, regex=None, work_dir=None):
+    xnat_list = {}
     with open(config_file) as f:
         config = json.load(f)
     session = xnat.connect(config['server'],
@@ -18,10 +18,14 @@ def xget_file(config_file=None, project=None, regex=None):
     for subject in session.projects[project].subjects:
         if not re.search(regex, session.projects[project].subjects[subject].label):
             continue
+        subject = session.projects[project].subjects[subject].label
+        xnat_list[subject] = []
         for exp in session.subjects[subject].experiments:
-            subses_label = session.subjects[subject].experiments[exp].label
-            xnat_list.append(subses_label)
-    print(xnat_list)
+            exp = session.subjects[subject].experiments[exp].label
+            xnat_list[subject].append(exp)
+            #subses_label = session.subjects[subject].experiments[exp].label
+    with open(work_dir + '/' + project + 'json', 'w') as df:
+        json.dump(xnat_list, df, indent=4)
 
 
     #select_exp = '/projects/' + project +'/experiments'
@@ -35,6 +39,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('Arguments required to pull files')
     parser.add_argument('-c', '--config', dest='config_file', required=True)
     parser.add_argument('-p', '--project', dest='project', required=True)
+    parser.add_argument('-w', dest='work_dir')
     parser.add_argument('-r', '--regex', dest='regex', required=True)
     args = parser.parse_args()
-    xget_file(args.config_file, args.project, args.regex)
+    xget_file(args.config_file, args.project, args.regex, args.work_dir)
