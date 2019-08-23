2071dab4962ef11262a651952e9d6697def96b49
Adam Kimbler <akimbler@fiu.edu>
2019-08-23 15:34:46 -0400

    Update xnat_list.py

--- xnat_list.py
+++ xnat_list.py
@@ -13,8 +13,8 @@ def xget_file(config_file=None, project=None, ref=None):
                            user='akimb009',
                            password='h*RJ!0J2F9HTN*XQ')
     for subject in session.projects[project].subjects:
-        for exp in session.subjects[subject].experiments:
-            xnat_list.append(exp.values)
+        for exp in session.subjects[subject].experiments.values():
+            xnat_list.append(exp)
     print(xnat_list)
 
 
